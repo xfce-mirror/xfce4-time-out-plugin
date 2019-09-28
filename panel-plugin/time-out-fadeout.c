ed197d5eea78ef84ec4e7ddea9a678fb7720989b
Andre Miranda <andreldm@xfce.org>
2019-09-28 18:03:44 -0300

    Copy fadeout code from xfce4-session

--- panel-plugin/time-out-fadeout.c
+++ panel-plugin/time-out-fadeout.c
@@ -26,92 +26,143 @@
 #include <gtk/gtk.h>
 #include <libxfce4panel/xfce-panel-plugin.h>
 
-#include "time-out-fadeout.h"
+#ifdef GDK_WINDOWING_X11
+#include <gdk/gdkx.h>
+#include <X11/Xlib.h>
+#include <X11/Xatom.h>
+#include <cairo-xlib.h>
+#endif
 
+#include "time-out-fadeout.h"
 
 
 #define COLOR "#b6c4d7"
 
 
 
-struct _TimeOutScreen
-{
-  GdkWindow *window;
-  GdkPixmap *backbuf;
-};
 
 struct _TimeOutFadeout
 {
-  GdkColor color;
-  GList   *screens;
+#ifdef GDK_WINDOWING_X11
+  Display *xdisplay;
+  Window  *xwindow;
+#endif
 };
+
+
+
+#ifdef GDK_WINDOWING_X11
+static Window
+time_out_fadeout_new_window (GdkDisplay *display,
+                             GdkScreen  *screen)
+{
+  XSetWindowAttributes  attr;
+  Display              *xdisplay;
+  Window                xwindow;
+  GdkWindow            *root;
+  GdkCursor            *cursor;
+  cairo_t              *cr;
+  gint                  width;
+  gint                  height;
+  GdkPixbuf            *root_pixbuf;
+  cairo_surface_t      *surface;
+  gulong                mask = 0;
+  gulong                opacity;
+  gboolean              composited;
+  gint                  scale;
+
+  gdk_error_trap_push ();
+
+  xdisplay = gdk_x11_display_get_xdisplay (display);
+  root = gdk_screen_get_root_window (screen);
+
+  width = gdk_window_get_width (root);
+  height = gdk_window_get_height (root);
+
+  composited = gdk_screen_is_composited (screen)
+               && gdk_screen_get_rgba_visual (screen) != NULL;
+
+  cursor = gdk_cursor_new_for_display (display, GDK_WATCH);
+
+  scale = gdk_window_get_scale_factor (root);
+  width *= scale;
+  height *= scale;
+
+  if (!composited)
+    {
+      /* create a copy of root window before showing the fadeout */
+      root_pixbuf = gdk_pixbuf_get_from_window (root, 0, 0, width, height);
+    }
+
+  attr.cursor = gdk_x11_cursor_get_xcursor (cursor);
+  mask |= CWCursor;
+
+  attr.override_redirect = TRUE;
+  mask |= CWOverrideRedirect;
+
+  attr.background_pixel = BlackPixel (xdisplay, gdk_x11_screen_get_screen_number (screen));
+  mask |= CWBackPixel;
+
+  xwindow = XCreateWindow (xdisplay, gdk_x11_window_get_xid (root),
+                           0, 0, width, height, 0, CopyFromParent,
+                           InputOutput, CopyFromParent, mask, &attr);
+
+  g_object_unref (cursor);
+
+  if (composited)
+    {
+      /* apply transparency before map */
+      opacity = 0.5 * 0xffffffff;
+      XChangeProperty (xdisplay, xwindow,
+                       gdk_x11_get_xatom_by_name_for_display (display, "_NET_WM_WINDOW_OPACITY"),
+                       XA_CARDINAL, 32, PropModeReplace, (guchar *)&opacity, 1);
+    }
+
+  XMapWindow (xdisplay, xwindow);
+
+  if (!composited)
+    {
+      /* create background for window */
+      surface = cairo_xlib_surface_create (xdisplay, xwindow,
+                                           gdk_x11_visual_get_xvisual (gdk_screen_get_system_visual (screen)),
+                                           0, 0);
+      cairo_xlib_surface_set_size (surface, width, height);
+      cr = cairo_create (surface);
+
+      /* draw the copy of the root window */
+      gdk_cairo_set_source_pixbuf (cr, root_pixbuf, 0, 0);
+      cairo_paint (cr);
+      g_object_unref (root_pixbuf);
+
+      /* draw black transparent layer */
+      cairo_set_source_rgba (cr, 0, 0, 0, 0.5);
+      cairo_paint (cr);
+      cairo_destroy (cr);
+      cairo_surface_destroy (surface);
+    }
+
+  gdk_flush ();
+  gdk_error_trap_pop_ignored ();
+
+  return xwindow;
+}
+#endif
 
 
 
 TimeOutFadeout*
 time_out_fadeout_new (GdkDisplay *display)
 {
-  TimeOutFadeout *fadeout;
-  GdkWindowAttr   attr;
-  GdkGCValues     values;
-  GdkWindow      *root;
-  GdkCursor      *cursor;
-  TimeOutScreen  *screen;
-  GdkGC          *gc;
-  GList          *iter;
-  cairo_t        *cr;
-  gint            width;
-  gint            height;
-  gint            n;
+  TimeOutFadeout  *fadeout;
+  GdkScreen       *screen;
 
-  fadeout = panel_slice_new0 (TimeOutFadeout);
-  gdk_color_parse (COLOR, &fadeout->color);
+  fadeout = g_slice_new0 (TimeOutFadeout);
 
-  cursor = gdk_cursor_new (GDK_WATCH);
-
-  attr.x = 0;
-  attr.y = 0;
-  attr.event_mask = 0;
-  attr.wclass = GDK_INPUT_OUTPUT;
-  attr.window_type = GDK_WINDOW_TEMP;
-  attr.cursor = cursor;
-  attr.override_redirect = TRUE;
-
-  for (n = 0; n < gdk_display_get_n_screens (display); ++n) 
-    {
-      screen = panel_slice_new0 (TimeOutScreen);
-
-      root = gdk_screen_get_root_window (gdk_display_get_screen (display, n));
-      gdk_drawable_get_size (GDK_DRAWABLE (root), &width, &height);
-
-      values.function = GDK_COPY;
-      values.graphics_exposures = FALSE;
-      values.subwindow_mode = TRUE;
-      gc = gdk_gc_new_with_values (root, &values, GDK_GC_FUNCTION | GDK_GC_EXPOSURES | GDK_GC_SUBWINDOW);
-
-      screen->backbuf = gdk_pixmap_new (GDK_DRAWABLE (root), width, height, -1);
-      gdk_draw_drawable (GDK_DRAWABLE (screen->backbuf), gc, GDK_DRAWABLE (root), 0, 0, 0, 0, width, height);
-
-      cr = gdk_cairo_create (GDK_DRAWABLE (screen->backbuf));
-      gdk_cairo_set_source_color (cr, &fadeout->color);
-      cairo_paint_with_alpha (cr, 0.5);
-      cairo_destroy (cr);
-
-      attr.width = width;
-      attr.height = height;
-
-      screen->window = gdk_window_new (root, &attr, GDK_WA_X | GDK_WA_Y | GDK_WA_NOREDIR | GDK_WA_CURSOR);
-      gdk_window_set_back_pixmap (screen->window, screen->backbuf, FALSE);
-
-      g_object_unref (G_OBJECT (gc));
-
-      fadeout->screens = g_list_append (fadeout->screens, screen);
-    }
-
-  for (iter = fadeout->screens; iter != NULL; iter = g_list_next (iter))
-    gdk_window_show (((TimeOutScreen *) iter->data)->window);
-
-  gdk_cursor_unref (cursor);
+#ifdef GDK_WINDOWING_X11
+  fadeout->xdisplay = gdk_x11_display_get_xdisplay (display);
+  screen = gdk_display_get_default_screen (display);
+  fadeout->xwindow = GINT_TO_POINTER (time_out_fadeout_new_window (display, screen));
+#endif
 
   return fadeout;
 }
@@ -121,18 +172,12 @@ time_out_fadeout_new (GdkDisplay *display)
 void
 time_out_fadeout_destroy (TimeOutFadeout *fadeout)
 {
-  TimeOutScreen *screen;
-  GList         *iter;
+#ifdef GDK_WINDOWING_X11
+  gdk_error_trap_push ();
+  XDestroyWindow (fadeout->xdisplay, GPOINTER_TO_INT (fadeout->xwindow));
+  gdk_flush ();
+  gdk_error_trap_pop_ignored ();
+#endif
 
-  for (iter = fadeout->screens; iter != NULL; iter = g_list_next (iter))
-    {
-      screen = iter->data;
-
-      gdk_window_destroy (screen->window);
-      g_object_unref (G_OBJECT (screen->backbuf));
-      panel_slice_free (TimeOutScreen, screen);
-    }
-
-  g_list_free (fadeout->screens);
-  panel_slice_free (TimeOutFadeout, fadeout);
+  g_slice_free (TimeOutFadeout, fadeout);
 }
