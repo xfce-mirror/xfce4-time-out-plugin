60740191f1d5e4700fe6cd615681aec6b48de4cf
Bobby Rong <rjl931189261@126.com>
2025-03-23 23:18:31 +0800

    Add meson build

--- panel-plugin/time-out.c
+++ panel-plugin/time-out.c
@@ -22,6 +22,9 @@
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
+#ifdef HAVE_XFCE_REVISION_H
+#include "xfce-revision.h"
+#endif
 
 #include <gtk/gtk.h>
 #include <libxfce4util/libxfce4util.h>
@@ -432,7 +435,7 @@ time_out_about (XfcePanelPlugin *plugin)
                          "destroy-with-parent", TRUE,
                          "logo-icon-name", "xfce4-time-out-plugin",
                          "program-name", PACKAGE_NAME,
-                         "version", PACKAGE_VERSION,
+                         "version", VERSION_FULL,
                          "translator-credits", _("translator-credits"),
                          "license", XFCE_LICENSE_GPL,
                          "website", "https://docs.xfce.org/panel-plugins/xfce4-time-out-plugin",
