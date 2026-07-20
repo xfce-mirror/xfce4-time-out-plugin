af5068e27237a9afc372f5fc45d27a1ef71884ae
Gaël Bonithon <gael@xfce.org>
2026-07-20 20:08:59 +0200

    cppcheck: Fix unusedVariable

--- panel-plugin/time-out-fadeout.c
+++ panel-plugin/time-out-fadeout.c
@@ -149,16 +149,13 @@ time_out_fadeout_new_window (GdkDisplay *display,
 TimeOutFadeout*
 time_out_fadeout_new (GdkDisplay *display)
 {
-  TimeOutFadeout  *fadeout;
-  GdkScreen       *screen;
-
-  fadeout = g_slice_new0 (TimeOutFadeout);
+  TimeOutFadeout  *fadeout = g_slice_new0 (TimeOutFadeout);
 
 #if defined (GDK_WINDOWING_X11) && defined (HAVE_LIBX11)
   if (GDK_IS_X11_DISPLAY (display))
     {
       fadeout->xdisplay = gdk_x11_display_get_xdisplay (display);
-      screen = gdk_display_get_default_screen (display);
+      GdkScreen *screen = gdk_display_get_default_screen (display);
       fadeout->xwindow = GINT_TO_POINTER (time_out_fadeout_new_window (display, screen));
     }
 #endif
